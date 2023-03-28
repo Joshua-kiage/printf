f5c2c22ced2ee697b758f1a22f82ad8dc178b77e
PatriciaMkoji <keyshotrisha@gmail.com>
2023-03-28 07:06:06 -0700

    Header Files

--- main.h
+++ main.h
@@ -73,6 +73,7 @@ typedef struct printHandler
 /* utils.c */
 int _strlen(const char *);
 int print(char *);
+char *itoa(long int, int);
 
 /* print_nums */
 int print_int(va_list l, flags_t *f);
@@ -127,5 +128,8 @@ int print_rot(va_list);
 
 /* rev_string */
 int  print_rev_string(va_list list);
+
+/* get_precision */
+int get_precision(const char *format, int *i, va_list list);
 
 #endif
